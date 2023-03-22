2ff478d6a706978964553d984f580cb272d7d8f4
vennela jangam <vennela.jangam@nagarro.com>
2023-03-22 14:40:58 +0530

    first commit

---
+++ q2.cs
@@ -0,0 +1,10 @@
+﻿'ques1.exe' (CoreCLR: DefaultDomain): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.32\System.Private.CoreLib.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
+'ques1.exe' (CoreCLR: clrhost): Loaded 'C:\Users\vennelajangam\source\repos\ques1\ques1\bin\Debug\netcoreapp3.1\ques1.dll'. Symbols loaded.
+'ques1.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.32\System.Runtime.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
+'ques1.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.32\System.Collections.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
+'ques1.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.32\System.Linq.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
+'ques1.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.32\System.Runtime.Extensions.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
+'ques1.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.32\System.Console.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
+'ques1.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.32\System.Threading.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
+'ques1.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.32\System.Text.Encoding.Extensions.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
+The program '[15084] ques1.exe' has exited with code 0 (0x0).
---
+++ q3.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethodDemo
+{
+    static class Extensions
+    {
+        public static bool CustomAll<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (T item in source)
+            {
+                if (!predicate(item))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool CustomAny<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (T item in source)
+            {
+                if (predicate(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static T CustomMax<T>(this IEnumerable<T> source, Func<T, T, int> compare)
+        {
+            T max = source.FirstOrDefault();
+            foreach (T item in source.Skip(1))
+            {
+                if (compare(item, max) > 0)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+
+        public static T CustomMin<T>(this IEnumerable<T> source, Func<T, T, int> compare)
+        {
+            T min = source.FirstOrDefault();
+            foreach (T item in source.Skip(1))
+            {
+                if (compare(item, min) < 0)
+                {
+                    min = item;
+                }
+            }
+            return min;
+        }
+
+        public static IEnumerable<T> CustomWhere<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (T item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> CustomSelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            foreach (TSource item in source)
+            {
+                yield return selector(item);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5 };
+
+            bool allEven = numbers.CustomAll(x => x % 2 == 0);
+            Console.WriteLine("All even: " + allEven);
+
+            bool anyOdd = numbers.CustomAny(x => x % 2 != 0);
+            Console.WriteLine("Any odd: " + anyOdd);
+
+            int max = numbers.CustomMax((x, y) => x.CompareTo(y));
+            Console.WriteLine("Max: " + max);
+
+            int min = numbers.CustomMin((x, y) => x.CompareTo(y));
+            Console.WriteLine("Min: " + min);
+
+            IEnumerable<int> evenNumbers = numbers.CustomWhere(x => x % 2 == 0);
+            Console.WriteLine("Even numbers: " + string.Join(", ", evenNumbers));
+
+            IEnumerable<string> words = new List<string>() { "foo", "bar", "baz" };
+            IEnumerable<int> wordLengths = words.CustomSelect(x => x.Length);
+            Console.WriteLine("Word lengths: " + string.Join(", ", wordLengths));
+        }
+    }
+}
+
